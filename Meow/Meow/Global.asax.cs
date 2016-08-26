2a089b477b4cf2fad30801eb7abb9e88301c40cd
Markus Koschier <markus.koschier@cofinpro.de>
2016-08-26 14:01:26 +0200

    Bootstrap drin

--- Meow/Meow/Global.asax.cs
+++ Meow/Meow/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 using System.Web.Http;
+using System.Web.Optimization;
 
 namespace Meow
 {
@@ -17,7 +18,8 @@ namespace Meow
             // Code, der beim Anwendungsstart ausgeführt wird
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
