c3c78f39f5154fc0a9f4f3299f830c48129d8fa9
Markus Koschier <markus.koschier@cofinpro.de>
2016-08-27 12:32:40 +0200

    Following-Konzept erweitert

--- Meow/Meow/Controllers/FollowerController.cs
+++ Meow/Meow/Controllers/FollowerController.cs
@@ -5,18 +5,18 @@ using System.Web;
 using System.Web.Mvc;
 using Meow.Models.Follower;
 using Meow.Code.DAL;
+using Meow.Code.Model;
 
 namespace Meow.Controllers
 {
     public class FollowerController : Controller
     {
         private readonly MeowContext _context = new MeowContext();
+        //TODO Ersetzen mit getCurrentCat.getId o.ä.
+        private long ownId = 3;
 
-        // GET: List all users (being followed and not being followed)
         public ActionResult Index()
         {
-            //TODO Ersetzen mit getCurrentCat.getId o.ä.
-            long ownId = 1;
             var myFollowerIds = from f in _context.Follower where f.IsBeingFollowed.Equals(ownId) select f.IsFollowing;
             var myFollowerCats = from c in _context.Cats where myFollowerIds.ToList().Contains(c.Id) select c;
 
@@ -25,9 +25,13 @@ namespace Meow.Controllers
 
             return View(new FollowerModel() { CatsBeingFollowed = myFollowerCats.ToList(), CatsFollowing = myFollowingCats.ToList() });
         }
-        public ActionResult Follow(FollowerModel model)
+        public ActionResult Unfollow(long id)
         {
-            return View(model);
+            var FollowerToDelete = from f in _context.Follower where f.IsBeingFollowed.Equals(id) && f.IsFollowing.Equals(ownId) select f;
+            Follower Follower = FollowerToDelete.ToList()[0];
+            _context.Follower.Remove(Follower);
+            _context.SaveChanges();
+            return Redirect("/Follower");
         }
     }
 }
