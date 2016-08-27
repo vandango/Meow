8eb0fef5ea80963597567ff83c0f0d0ade3e4026
Markus Koschier <markus.koschier@cofinpro.de>
2016-08-27 13:38:30 +0200

    baseUrl ergänzt

--- Meow/Meow/App_Start/RouteConfig.cs
+++ Meow/Meow/App_Start/RouteConfig.cs
@@ -15,8 +15,8 @@ namespace Meow
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}/{id}/{returnUrl}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional, returnUrl = UrlParameter.Optional }
             );
         }
     }
